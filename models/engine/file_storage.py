679be48c85949686cfb73cba6b9ac76bf96feb80
antoineleno <johndoe@example.com>
2024-04-15 11:51:14 +0800

    RBNB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,13 +12,25 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """all: Method that return all the objects of a class
+
+        Returns:
+            dictionnary: dictionnary representation of all objects
+        """
         return self.__objects
 
     def new(self, obj):
+        """new: method to set in __objects the obj with key <obj class name>.id
+
+        Args:
+            obj (object): instance of a class
+        """
         key = "{}.{}".format(type(obj).__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        """save: Method to save an object to a private class attribure __objects
+        """
         all_objects = self.__objects
         dict_objects = {}
         for obj in all_objects.keys():
@@ -29,6 +41,10 @@ class FileStorage:
 
         
     def reload(self):
+        """
+            reload: method to deserialise an object from an existing file
+            then save it to private class attribute __object
+        """
         if os.path.isfile(self.__file_path):
             try:
                 with open(self.__file_path, "r") as file:
@@ -36,11 +52,8 @@ class FileStorage:
                     for key, value in content.items():
                         class_name, obj_id = key.split('.')
                         cls = eval(class_name)
-                        instance = cls(**value)
-                        self.__objects[key] = instance
+                        class_instance = cls(**value)
+                        self.__objects[key] = class_instance
             
             except Exception:
                 pass
-        
-    
-    
