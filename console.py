631b7055e2330385f03d032277688a7a907f13a1
antoineleno <johndoe@example.com>
2024-04-15 18:52:56 +0800

    RBNB

--- console.py
+++ console.py
@@ -2,22 +2,25 @@
 """
 console module
 """
+
 import cmd
+
 
 class BNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    def help_quit(self):
-        print("Quit command to exit the program")
-
     def do_quit(self, line):
+        """Quit command to exit the program"""
         return True
+
+    def emptyline(self):
+        """Called when an empty line is entered"""
+        print(end="")
 
     def do_EOF(self, line):
+        """Handle End-of-File (EOF) condition to exit the program gracefully"""
         print()
         return True
-
-
 
 
 if __name__ == '__main__':
