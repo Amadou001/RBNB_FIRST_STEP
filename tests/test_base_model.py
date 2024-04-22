65d1a5d095daa4b87cf7d519f0729655327ae31a
antoineleno <johndoe@example.com>
2024-04-23 03:35:06 +0800

    RBNB

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -11,9 +11,12 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """Class to test basemodel module"""
-    
+
     def test_create_object(self):
-        """Check wether it's possible to create an object from BaseModel class"""
+        """
+        Check wether it's possible to create
+        an object from BaseModel class
+        """
         my_new_modele = BaseModel()
         self.assertIsInstance(my_new_modele, BaseModel)
 
@@ -24,13 +27,12 @@ class TestBaseModel(unittest.TestCase):
         my_model.my_number = 89
         self.assertEqual(my_model.name, "My First Model")
         self.assertEqual(my_model.my_number, 89)
-    
+
     def test_kwargs_case(self):
         """Test kwargs case"""
-        my_mode = BaseModel(name = "Antoine", age = 55)
+        my_mode = BaseModel(name="Antoine", age=55)
         self.assertEqual(my_mode.name, "Antoine")
         self.assertEqual(my_mode.age, 55)
-        
 
     def test_id_attribute(self):
         """Test all model class attribute"""
@@ -48,29 +50,31 @@ class TestBaseModel(unittest.TestCase):
         """Test updated at attribute"""
         my_model = BaseModel()
         self.assertIsInstance(my_model.updated_at, datetime)
-        
 
     def test_str_representation(self):
         """Test str representation"""
-        my_model = BaseModel()
-        expected_output = f"[{my_model.__class__.__name__}] ({my_model.id}) {my_model.__dict__}"
-        self.assertEqual(str(my_model), expected_output)
+        my_mo = BaseModel()
+        ex_op = f"[{my_mo.__class__.__name__}] ({my_mo.id}) {my_mo.__dict__}"
+        self.assertEqual(str(my_mo), ex_op)
 
     def test_str_representation_with_arguments(self):
         """Test str representation while providing arguments"""
-        my_model = BaseModel()
-        my_model.my_list = [1, 2, 4]
-        expected_output = f"[{my_model.__class__.__name__}] ({my_model.id}) {my_model.__dict__}"
-        self.assertEqual(str(my_model), expected_output)
-    
+        my_ml = BaseModel()
+        my_ml.my_list = [1, 2, 4]
+        output = f"[{my_ml.__class__.__name__}] ({my_ml.id}) {my_ml.__dict__}"
+        self.assertEqual(str(my_ml), output)
+
     def test_base_model_with_argument(self):
-        """Test basemodel with argument when trying to access class attributes"""
+        """
+        Test basemodel with argument when
+        trying to access class attributes
+        """
         with self.assertRaises(AttributeError):
-            my_model = BaseModel(name = "Antoine", age = 55)
+            my_model = BaseModel(name="Antoine", age=55)
             uuid_obj = uuid.UUID(my_model.id)
             self.assertTrue(uuid.UUID(my_model.id, version=4))
             self.assertIsInstance(uuid_obj, uuid.UUID)
-    
+
     def test_to_dict_no_attribute(self):
         """Test to dict method"""
         my_model = BaseModel()
@@ -126,11 +130,10 @@ class TestBaseModel(unittest.TestCase):
         expected_dict.update(dictionary)
         self.assertDictEqual(my_model_dict_repr, expected_dict)
 
-
     def test_to_dict_with_kwargs(self):
         """Test to dict method"""
         with self.assertRaises(AttributeError):
-            my_model = BaseModel(name = "My first model", number = 65)
+            my_model = BaseModel(name="My first model", number=65)
             my_model_dict_repr = my_model.to_dict()
             class_name = f"{my_model.__class__.__name__}"
             dictionary = my_model.__dict__
@@ -146,14 +149,9 @@ class TestBaseModel(unittest.TestCase):
     def test_update_at_created_at_assignemt(self):
         """test update_at and created_at"""
         with self.assertRaises(ValueError):
-            my_model = BaseModel(created_at = "12pm", updated_at = "12pm")
+            my_model = BaseModel(created_at="12pm", updated_at="12pm")
             self.assertEqual(my_model.created_at, "12pm")
 
-    
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
-
