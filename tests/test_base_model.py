e1b8584a6919568abb2c17fb8427fc86d4f45a14
antoineleno <johndoe@example.com>
2024-04-23 02:49:02 +0800

    RBNB

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -49,13 +49,52 @@ class TestBaseModel(unittest.TestCase):
         expected_output = f"[{my_model.__class__.__name__}] ({my_model.id}) {my_model.__dict__}"
         self.assertEqual(str(my_model), expected_output)
 
+    def test_str_repr_with_argument(self):
+        """Test str with argument"""
+        with self.assertRaises(AttributeError):
+            my_model = BaseModel(name = "Antoine", age = 55)
+            uuid_obj = uuid.UUID(my_model.id)
+            self.assertTrue(uuid.UUID(my_model.id, version=4))
+            self.assertIsInstance(uuid_obj, uuid.UUID)
+    
+    def test_to_dict_no_attribute(self):
+        """Test to dict method"""
+        my_model = BaseModel()
+        my_model_dict_repr = my_model.to_dict()
+        class_name = f"{my_model.__class__.__name__}"
+        dictionary = my_model.__dict__
+        expected_dict = {"__class__": class_name}
+        expected_dict.update(dictionary)
+        self.assertDictEqual(my_model_dict_repr, expected_dict)
+
+    def test_to_dict_with_attribute(self):
+        """Test to dict method"""
+        my_model = BaseModel()
+        my_model.name = "My first model"
+        my_model.number = 33
+        my_model_dict_repr = my_model.to_dict()
+        class_name = f"{my_model.__class__.__name__}"
+        dictionary = my_model.__dict__
+        expected_dict = {"__class__": class_name}
+        expected_dict.update(dictionary)
+        self.assertDictEqual(my_model_dict_repr, expected_dict)
+
+    def test_to_dict_with_kwargs(self):
+        """Test to dict method"""
+        with self.assertRaises(AttributeError):
+            my_model = BaseModel(name = "My first model", number = 65)
+            my_model_dict_repr = my_model.to_dict()
+            class_name = f"{my_model.__class__.__name__}"
+            dictionary = my_model.__dict__
+            expected_dict = {"__class__": class_name}
+            expected_dict.update(dictionary)
+
     def test_save_method_without_storage(self):
         """Test save method without storage"""
-        pass
+        my_model = BaseModel()
+        my_model.save()
+        self.assertIsInstance(my_model.updated_at, datetime)
 
-    def test_to_dict(self):
-        """Test to dict method"""
-        pass
 
 
 
